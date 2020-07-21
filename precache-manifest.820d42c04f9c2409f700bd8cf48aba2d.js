f594ef3d03c6a5c8d5c529248a554ed5e057c766
Yohan Tancrez <yohan.tancrez@epita.fr>
2020-07-21 20:24:53 +0200

    Updates

---
+++ precache-manifest.820d42c04f9c2409f700bd8cf48aba2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640edbbc7675e46fc61ce2d4870f38e8",
+    "url": "/Sorting-Visualizer/index.html"
+  },
+  {
+    "revision": "8a30f955c3ada7449d4b",
+    "url": "/Sorting-Visualizer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b618014ba40c7e460ad1",
+    "url": "/Sorting-Visualizer/static/js/2.c816e63c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sorting-Visualizer/static/js/2.c816e63c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a30f955c3ada7449d4b",
+    "url": "/Sorting-Visualizer/static/js/main.3f6ebcf8.chunk.js"
+  },
+  {
+    "revision": "37f1b58fcf10c98f1c1c",
+    "url": "/Sorting-Visualizer/static/js/runtime-main.383369f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sorting-Visualizer/static/media/logo.5d5d9eef.svg"
+  }
+]);
